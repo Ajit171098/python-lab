72551375483980290aac56f6c4a88ec47d52be1e
Ajit171098 <45589832+Ajit171098@users.noreply.github.com>
2019-08-29 12:54:30 +0530

    Update 2(a).py

--- 2(a).py
+++ 2(a).py
@@ -5,11 +5,11 @@ def search1(list1,k):
        return (0)
 n=int(input("Enter the no of elements in the list\n"))
 list2=[]
-no=int(input("Enter the no to be searched\n"))
 print("Enter elements")
 for i in range(0,n):
     a=int(input())
     list2.append(a)
+no=int(input("Enter the no to be searched\n"))
 result=search1(list2,no)
 print(result)
     
